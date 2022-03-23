b54e69fd8a5ae546ad10594cd59625e9bb600e3d
Adenialzz <1160180309@qq.com>
2022-03-23 10:31:49 +0800

    update

--- kornia_test.py
+++ kornia_test.py
@@ -1,17 +1,22 @@
 import torch
 import kornia.enhance as ke
 import cv2
-from SongUtils import IOUtils as iout
+from utils.utils import *
 
 
-inputs = iout.readTensorImage("/ssd1t/song/Datasets/AVA/shortEdge256/125.jpg", through='opencv')
-# outputs = ke.adjust_brightness(inputs, 0.8)   # [-0.5, 0.5], origin: 0.
+from PIL import Image
+import torchvision.transforms as transforms
+pipeline = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+inten = -0.5
+inputs = pipeline(Image.open('imgs/125.jpg').convert('RGB'))
+outputs = ke.adjust_brightness(inputs, inten)   # [-0.5, 0.5], origin: 0.
 # outputs = ke.adjust_contrast(inputs, 1.)      # [0.2, 1.8], 1.
 # outputs = ke.adjust_hue(inputs, -2.)            # [-3., 3.], 0.
-# outputs = ke.adjust_saturation(inputs, 0.)      # [0., 5.]. 1.
+# outputs = ke.adjust_saturation(inputs, 10.)      # [0., 5.]. 1.
 # outputs = ke.adjust_gamma(inputs, 1.8)             # [0.2, 1.8], 1.
 
-img = iout.tensor2cv(outputs)
-print(img.shape)
-cv2.imwrite('edited.jpg', img[:,:,::-1])
+save_tensor_image(outputs, f"imgs/kornia_test-{inten}.jpg")
 
--- test_nicer_can.py
+++ test_nicer_can.py
@@ -1,21 +1,26 @@
 import torch
 import torchvision.transforms as transforms
+import torch.nn.functional as F
 import numpy as np
 import cv2
 from PIL import Image
 
 from models.nicer_models import CAN
+from models.sit import VisionTransformer_SiT
 from utils.utils import load_weights, save_tensor_image
 mse_loss_func = torch.nn.MSELoss()
 
 def test_can():
     num_filters = 5
     can = CAN(num_filters) 
+    # can = VisionTransformer_SiT(in_chans=8, num_filters=0)
     # weights_path = 'weights/nicer/can8_epoch10_final.pt'
-    weights_path = '/media/song/ImageEnhancingResults/model/can_cnn/model_3.pth'
+    weights_path = 'ImageEnhancingResults/model/can_cnn_lr1e-2/model_90.pth'
+    # weights_path = '/media/song/ImageEnhancingResults/model/can_vit/model_17.pth'
     can = load_weights(can, weights_path)
 
     # strings = ['Sat', 'Con', 'Bri', 'Sha', 'Hig', 'LLF', 'NLD', 'EXP']
+    # strings = ['Bri', 'gamma', 'Con', 'Sat', 'Hue']
     filter_intensities = [0., 0., 0., 0., 0., 0., 0., 0.][: num_filters]
     filter_channels = None
     for inten in filter_intensities:
@@ -24,6 +29,7 @@ def test_can():
         if filter_channels is None:
             filter_channels = channel
         else:
+            # filter_channels = torch.cat((filter_channels, channel), dim=1)
             filter_channels = torch.cat((filter_channels, channel), dim=1)
     pipeline = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -32,12 +38,11 @@ def test_can():
     img = Image.open("imgs/125.jpg").convert('RGB')
     tensor_img = pipeline(img)
     inputs = torch.cat((tensor_img.unsqueeze(dim=0), filter_channels), dim=1)
-    print(inputs.shape)
 
     outputs = can(inputs).detach()
     outputs = outputs.squeeze(dim=0)
     print(mse_loss_func(tensor_img, outputs).item())
-    save_tensor_image(outputs, "imgs/nicer_edited.jpg")
+    save_tensor_image(outputs+tensor_img, f"imgs/test_nicer_edited.jpg")
 
 
 if __name__ == "__main__":
